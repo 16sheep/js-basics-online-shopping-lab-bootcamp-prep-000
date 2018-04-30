7ec25cba22c3524435c383920be6eac5597ddf46
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-30 10:44:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ function addToCart(item) {
 
 function viewCart() {
   let cartReport = "In your cart you have "
+  if (cart.length === 0) {
+    return 
+  }
   for(let i = 0; i < cart.length; i++){
     if (i === 0) {
       cartReport = "${cartreport}${item} at ${cart[i][itemPrice]}"
@@ -28,8 +31,11 @@ function viewCart() {
     else if(i === cart.length - 1){
       cartReport = "and ${item} at ${cart[i][itemPrice]}."
     }
-    
+    else {
+      cartReport = ", ${item} at ${cart[i][itemPrice]}"
+    }
   }
+  return cartReport
 }
 
 function total() {
