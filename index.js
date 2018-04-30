72a3da7f5849f46ab434a40db7855d403597a74e
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-30 11:42:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function removeFromCart(item) {
   // write your code here
   for(let i = 0; i < cart.length; i++){
     if (cart[i].itemName === item){
-      delete cart[i][itemName];
+      delete cart[i].itemName;
       return cart[i];
     }
   }
