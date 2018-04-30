62e94cddb015aed9c17b4fcda501cf18b38d6686
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-30 10:58:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,24 +26,35 @@ function viewCart() {
   }
   for(let i = 0; i < cart.length; i++){
     if (i === 0) {
-      cartReport = "${cartreport}${item} at ${cart[i][itemPrice]}"
+      cartReport = "${cartreport}${item} at ${cart[i]['itemPrice']}"
     }
     else if(i === cart.length - 1){
-      cartReport = "and ${item} at ${cart[i][itemPrice]}."
+      cartReport = "and ${item} at ${cart[i]['itemPrice']}."
     }
     else {
-      cartReport = ", ${item} at ${cart[i][itemPrice]}"
+      cartReport = ", ${item} at ${cart[i]['itemPrice']}"
     }
   }
   return cartReport
 }
 
 function total() {
-  // write your code here
+  let sum = 0
+  for(let i = 0; i < cart.length; i++){
+    sum = sum + cart[i]['itemPrice']
+  }
+  return sum
 }
 
 function removeFromCart(item) {
   // write your code here
+  for(let i = 0; i < cart.length; i++){
+    if (cart[i]['itemName'] === item){
+      delete cart[i][itemName]
+      return cart[i]
+    }
+  }
+  return 
 }
 
 function placeOrder(cardNumber) {
