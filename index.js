2390f3045c6287dc5c4d81627d5e6114ab3fb62b
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-30 11:03:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,9 +61,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === false){
     return "Sorry, we don't have a credit card on file for you."
   }
-  else {
-    sum = total();
-    cart.length = 0
-    return 
-  }
+  sum = total();
+  cart.length = 0
+  return "Your total cost is $${sum}, which will be charged to the card ${cardNumber}.
 }
